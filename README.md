09dd9adc9d01ef44edf38a426ce9e222cffe1c1e
Gil Herr <gilherr@gmail.com>
2020-08-02 17:25:49 +0300

    minor cleanup

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Giler's dotfiles
 
-For more info see the [wiki](https://github.com/gilherr/dotfiles/wiki).
+...
--- install.sh
+++ install.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
+end_color='\033[0m';red='\033[0;31m';
+green='\033[0;32m';yellow='\033[1;33m';
+function color_echo { echo -e $1$2${end_color}; }
+
 if [ -z "$DOTFILES" ]
 then
-      echo "Error: Cant find dotfiles location. Missing \$DOTFILES env var"
+      color_echo $red "Error: Cant find dotfiles location. Missing \$DOTFILES env var"
       exit 1
 else
       echo "Found dotfiles path at: $DOTFILES"
 fi
 
+color_echo $green "Installing curl and git"
 sudo apt update && apt install -y curl git
 
+color_echo $green "Running zsh installer"
 $DOTFILES/zsh/install.sh
+
+color_echo $green "Running vim installer"
 $DOTFILES/vim/install.sh
 
-echo Done.
+color_echo $green "Done."
