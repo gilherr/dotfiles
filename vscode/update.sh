284928db08b2fb280044d0c0ef785373e9689e87
gilherr <gilherr@gmail.com>
2021-11-15 09:53:43 +0200

    update vscode settings

--- vscode/update.sh
+++ vscode/update.sh
@@ -1,1 +1,4 @@
+#!/usr/bin/env bash
+cp keybindings.json keybindings.json.BAK
+cp settings.json settings.json.BAK
 cp /mnt/c/Users/gil.herr/AppData/Roaming/Code/User/keybindings.json /mnt/c/Users/gil.herr/AppData/Roaming/Code/User/settings.json .
