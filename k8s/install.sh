1d53671c3d136dccc3c8a903ea7ca9f33383a32f
gilherr <gilherr@gmail.com>
2022-08-28 14:34:49 +0300

    add k8s krew and plugins installations

---
+++ k8s/install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+source ../shared/color.sh
+
+printf "${GREEN}\nInstall krew plugin\n${NORMAL}"
+
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
+
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+
+printf "${GREEN}Update krew plugin using kubectl\n${NORMAL}"
+kubectl krew update
+
+printf "${GREEN}Installing plugins\n${NORMAL}"
+kubectl krew install \
+    modify-secret
